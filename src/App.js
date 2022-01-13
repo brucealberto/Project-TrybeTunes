277eed0d690cd3b9d87ddec179ba9e2a756a998e
bruce <brucealberto2@gmail.com>
2022-01-13 14:28:51 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,62 @@
 import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+
+        <Route path="/album/:id">
+          <div data-testid="page-album">
+            <Album />
+          </div>
+        </Route>
+
+        <Route path="/">
+          <div data-testid="page-login">
+            <Login />
+          </div>
+        </Route>
+
+        <Route path="/search">
+          <div data-testid="page-search">
+            <Search />
+          </div>
+        </Route>
+
+        <Route path="/favorites">
+          <div data-testid="page-favorites">
+            <Favorites />
+          </div>
+        </Route>
+
+        <Route path="/profile">
+          <div data-testid="page-profile">
+            <Profile />
+          </div>
+        </Route>
+
+        <Route path="/profile/edit">
+          <div data-testid="page-profile-edit">
+            <ProfileEdit />
+          </div>
+        </Route>
+
+        <Route path="*">
+          <div data-testid="page-not-found">
+            <NotFound />
+          </div>
+        </Route>
+
+      </BrowserRouter>
+    );
   }
 }
 
