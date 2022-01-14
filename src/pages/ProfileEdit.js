3dc2004deb286a37207ff08a53172f1947831b6c
bruce <brucealberto2@gmail.com>
2022-01-14 18:53:00 -0300

    correcao

--- src/pages/ProfileEdit.js
+++ src/pages/ProfileEdit.js
@@ -4,7 +4,7 @@ import Header from '../components/Header';
 export default class ProfileEdit extends Component {
   render() {
     return (
-      <div data-testid="page-profile-edit">
+      <div>
         <Header />
         ProfileEdit
       </div>
