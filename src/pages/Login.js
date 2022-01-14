07bc0a2547b20771abbd3694047d6b2e9d8adc2b
bruce <brucealberto2@gmail.com>
2022-01-14 16:01:19 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,40 +1,86 @@
+import propTypes from 'prop-types';
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 export default class Login extends Component {
   constructor() {
     super();
 
     this.state = {
-      // name: '',
+      name: '',
+      notVisible: true,
+      load: false,
+      userValid: false,
     };
   }
 
+  componentWillUnmount() {
+    this.setState(() => {});
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ name: target.value }, () => this.validText());
+  };
+
+  validText = () => {
+    const { name } = this.state;
+    const maxNumber = 3;
+    if (name.length < maxNumber) {
+      this.setState({ notVisible: true });
+    } else {
+      this.setState({ notVisible: false });
+    }
+  };
+
+  callApi = () => {
+    const { name } = this.state;
+    this.setState({ load: true }, async () => {
+      await createUser({ name });
+
+      this.setState({ load: false, userValid: true });
+    });
+  };
+
   render() {
-    const validButton = (event) => {
-      event.preventDefault();
-      this.setState();
-      console.log(event.target);
-    };
+    const { name, notVisible, load, userValid } = this.state;
     return (
       <div>
-        <form>
-          <label htmlFor="nome">
-            <input
-              type="text"
-              data-testid="login-name-input"
-              id="nome"
-              placeholder="Nome"
-            />
-          </label>
-          <button
-            type="submit"
-            data-testid="login-submit-button"
-            onClick={ validButton }
-          >
-            Entrar
-          </button>
-        </form>
+
+        {userValid && <Redirect to="/search" />}
+        {load ? (
+          <Loading />
+        ) : (
+          <form>
+            <label htmlFor="nome">
+              <input
+                type="text"
+                name="text"
+                data-testid="login-name-input"
+                id="nome"
+                placeholder="Nome"
+                value={ name }
+                onChange={ this.handleChange }
+              />
+            </label>
+            <button
+              type="button"
+              name="button"
+              data-testid="login-submit-button"
+              onClick={ this.callApi }
+              disabled={ notVisible }
+            >
+              Entrar
+            </button>
+          </form>
+
+        )}
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  name: propTypes.string,
+}.isRequired;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,28 @@
+import propTypes from 'prop-types';
 import React, { Component } from 'react';
 
 export default class Search extends Component {
   render() {
+    const { name } = this.props;
+      // catchRequisition = async() => {
+      //   const catchApi = fetch(searchAlbumAPI);
+      // }
     return (
-      <div>
-        Search
-      </div>
-    );
-  }
+      <form>
+        <input
+          type="text"
+          placeholder="Nome do Artista"
+          data-testid="search-artist-input"
+          value={ name }
+        />
+
+        <button type="button" data-testid="search-artist-button">Pequisar</button>
+      </form>
+
+);
 }
+}
+
+Search.propTypes = {
+  name: propTypes.string,
+}.isRequired;
