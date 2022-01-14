6b72966e294aae5d709bc480c9ff723eeb127445
bruce <brucealberto2@gmail.com>
2022-01-14 19:29:34 -0300

    Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,22 +3,50 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      notVisible: true,
+      name: '',
+    };
+  }
+
+  handleButton = () => {
+    const { name } = this.state;
+    const maxNumber = 2;
+    if (name.length < maxNumber) {
+      this.setState({ notVisible: true });
+    } else {
+      this.setState({ notVisible: false });
+    }
+  };
+
+  handleName = ({ target }) => {
+    this.setState({ name: target.value }, this.handleButton);
+  };
+
   render() {
-    const { name } = this.props;
-    // catchRequisition = async() => {
-    //   const catchApi = fetch(searchAlbumAPI);
-    // }
+    const { name, notVisible } = this.state;
     return (
       <>
         <form>
           <input
             type="text"
+            name="text"
             placeholder="Nome do Artista"
             data-testid="search-artist-input"
+            onChange={ this.handleName }
             value={ name }
           />
 
-          <button type="button" data-testid="search-artist-button">Pequisar</button>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ notVisible }
+          >
+            Pequisar
+          </button>
         </form>
         <Header />
       </>
